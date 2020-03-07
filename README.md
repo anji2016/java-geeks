82ccc33dc5da2226eac33217232fec59a7ebc8cb
anji <m.anjaneyulu24@gmail.com>
2020-03-07 21:58:25 +0530

    about class , data types & object oriented programming

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # java-geeks
 To learn basics of java 
+
+Main components of java:
+	 1. class  - A  class consists of variables and methods
+	 2. variable - It is used to store and access data  ( state) 
+	 3. method - to implement functionality(logic or behavior ) 
+	 4. interface ( its like class but with some special features)
+	 
---
+++ src/com/java/basics/variables/Variables.java
@@ -0,0 +1,13 @@
+package com.java.basics.variables;
+
+public class Variables {
+    //syntax to declare a variable 
+	// dataType variableName = value;
+	int number = 30000; // short , int , long, float, double  - built in data types
+	double price = 23.2;
+	long population = 1300000000;
+	String name = "java-geeks"; // here String is class
+
+	// Note : java is not 100% object oriented languages
+	// We create objects from classes
+}
