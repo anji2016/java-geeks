5e05f34721368fc75e7b34bec6da9fb9b1eca673
anji <m.anjaneyulu24@gmail.com>
2020-06-21 15:32:00 +0530

    string comparison

---
+++ src/com/java/basics/string/programs/CompareTwoStrings.java
@@ -0,0 +1,27 @@
+package com.java.basics.string.programs;
+
+public class CompareTwoStrings {
+
+	
+	public static void main(String[] args) {
+		
+		String firstString = "aa";
+		String secondString = "aa";
+		
+		
+		//System.out.println(firstString.compareTo(secondString)); // -ve 
+		
+		System.out.println(firstString == secondString);
+		
+		
+		//String firstNumber = "100";
+		//String secondNumber = "900";
+	  
+		//System.out.println(firstString.equals(secondString));
+		//System.out.println(secondString.compareTo(firstString)); // +ve
+		
+		//System.out.println(firstNumber.compareTo(secondNumber)); // -1
+		//System.out.println(secondNumber.compareTo(firstNumber)); // +1
+		
+	}
+}
